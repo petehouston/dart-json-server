eec00cb513e23075feca6a41ee233b203d7eb99e
Pete Houston <contact@petehouston.com>
2019-11-01 01:30:09 -0500

    Add sample for HTTP server

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,15 @@
 import 'package:json_server/json_server.dart' as json_server;
+import 'dart:io';
 
-main(List<String> arguments) {
-  print('JSON Server');
+main() async {
+  var server = await HttpServer.bind(InternetAddress.loopbackIPv4, 8080);
+  print("Serving at ${server.address}:${server.port}");
+
+  await for (var request in server) {
+
+    request.response
+      ..headers.contentType = ContentType("text", "plain", charset: "utf-8")
+      ..write(request.uri.path);
+    await request.response.close();
+  }
 }
