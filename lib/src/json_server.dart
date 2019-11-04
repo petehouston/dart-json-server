fe58030c2ef9cc20de9ea961542cd0f7d9efd649
Pete Houston <contact@petehouston.com>
2019-11-03 18:04:27 -0600

    Error handler and log

--- lib/src/json_server.dart
+++ lib/src/json_server.dart
@@ -5,6 +5,10 @@ import 'dart:io';
 const keyData = 'data';
 const keyHost = 'host';
 const keyPort = 'port';
+
+const defaultHost = '127.0.0.1';
+const defaultPort = 1711;
+
 const res404 = '''
 {
   "message": "not found"
@@ -36,14 +40,12 @@ class JsonServer {
 
     // init server
     if (!this.config.containsKey(keyHost)) {
-      this.config[keyHost] = '127.0.0.1';
+      this.config[keyHost] = defaultHost;
     }
 
     if (!this.config.containsKey(keyPort)) {
-      this.config[keyPort] = '8080';
+      this.config[keyPort] = defaultPort as String;
     }
-
-    this.server = await HttpServer.bind(InternetAddress.loopbackIPv4, int.parse(this.config[keyPort]));
 
     // set init
     this.initialized = true;
@@ -59,17 +61,31 @@ class JsonServer {
   }
 
   _requestHandler(req) async {
-    req.response
-      ..headers.contentType = ContentType('application', 'json', charset: 'utf-8');
+    try {
+      this.server = await HttpServer.bind(
+        this.config[keyHost],
+        int.parse(this.config[keyPort]),
+        v6Only: false,
+      );
 
-    var apiPath = req.requestedUri.path;
+      stdout.write('Server is started at http://${config[keyHost]}:${config[keyPort]}');
 
-    if (!this.database.containsKey(apiPath)) {
-      req.response.write('{"error": "API not found"}');
-    } else {
-      req.response.write(jsonEncode(this.database[apiPath]));
+      req.response
+        ..headers.contentType = ContentType('application', 'json', charset: 'utf-8');
+
+      var apiPath = req.requestedUri.path;
+
+      if (!this.database.containsKey(apiPath)) {
+        req.response.statusCode = HttpStatus.notFound;
+        req.response.write(res404);
+      } else {
+        req.response.write(jsonEncode(this.database[apiPath]));
+      }
+
+      await req.response.close();
+    } catch (e) {
+      stderr.write('Error: ${e}\n');
+      exit(2);
     }
-
-    await req.response.close();
   }
 }
